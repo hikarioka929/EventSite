a5262988643410888b4b7c6bcb2dc1096bc460e7
piyoppa <piyoppa@gmail.com>
2021-01-25 05:15:08 +0900

    add login function

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,8 @@
-Rails.application.config.middleware.use OmuniAuth::Builder do
+Rails.application.config.middleware.use OmniAuth::Builder do
     if Rails.env.development? || Rails.env.test?
         provider :github, "73384ff4e636c45ba6b6", "a05bb3759cd02638a845980a8c4fc7f4e414fc05"
     else
-        provide :github,
+        provider :github,
         Rails.application.credentials.github[:client_id],
         Rails.application.credentials.github[:client_secret]
     end
